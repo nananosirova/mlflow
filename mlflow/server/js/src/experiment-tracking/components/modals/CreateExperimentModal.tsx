b0e92b35f008c72c374ed0930438bf41d24dd450
Nana Nosirova <10577112+nananosirova@users.noreply.github.com>
2026-05-04 10:39:22 -0400

    Add event tracking to MLflow midstream - HIGH
    Signed-off-by: Nana Nosirova <10577112+nananosirova@users.noreply.github.com>

--- mlflow/server/js/src/experiment-tracking/components/modals/CreateExperimentModal.tsx
+++ mlflow/server/js/src/experiment-tracking/components/modals/CreateExperimentModal.tsx
@@ -18,6 +18,8 @@ import { getExperimentNameValidator } from '../../../common/forms/validations';
 import { createExperimentApi } from '../../actions';
 import { getExperiments } from '../../reducers/Reducers';
 import { withRouterNext } from '../../../common/utils/withRouterNext';
+import { fireFormTrackingEvent, getTrackingError } from '../../../odh/analytics/segmentUtils';
+import { TrackingOutcome, MLflowEventNames } from '../../../odh/analytics/trackingProperties';
 
 type CreateExperimentModalImplProps = {
   isOpen?: boolean;
@@ -30,21 +32,31 @@ type CreateExperimentModalImplProps = {
 
 export class CreateExperimentModalImpl extends Component<CreateExperimentModalImplProps> {
   handleCreateExperiment = async (values: any) => {
-    // get values of input fields
     const experimentName = values[EXP_NAME_FIELD];
     const artifactLocation = values[ARTIFACT_LOCATION];
 
-    // createExperimentApi call needs to be fulfilled before redirecting the user to the newly
-    // created experiment page (history.push())
-    const response = await this.props.createExperimentApi(experimentName, artifactLocation);
-    this.props.onExperimentCreated();
+    try {
+      const response = await this.props.createExperimentApi(experimentName, artifactLocation);
+      this.props.onExperimentCreated();
 
-    const {
-      value: { experiment_id: newExperimentId },
-    } = response;
+      const {
+        value: { experiment_id: newExperimentId },
+      } = response;
 
-    if (newExperimentId) {
-      this.props.navigate(Routes.getExperimentPageRoute(newExperimentId));
+      if (newExperimentId) {
+        this.props.navigate(Routes.getExperimentPageRoute(newExperimentId));
+      }
+      fireFormTrackingEvent(MLflowEventNames.EXPERIMENT_CREATED, {
+        outcome: TrackingOutcome.submit,
+        success: true,
+      });
+    } catch (e: any) {
+      fireFormTrackingEvent(MLflowEventNames.EXPERIMENT_CREATED, {
+        outcome: TrackingOutcome.submit,
+        success: false,
+        error: getTrackingError(e),
+      });
+      throw e;
     }
   };
 
@@ -62,6 +74,11 @@ export class CreateExperimentModalImpl extends Component<CreateExperimentModalIm
         isOpen={isOpen}
         handleSubmit={this.handleCreateExperiment}
         onClose={this.props.onClose}
+        onCancel={() => {
+          fireFormTrackingEvent(MLflowEventNames.EXPERIMENT_CREATED, {
+            outcome: TrackingOutcome.cancel,
+          });
+        }}
       >
         {/* @ts-expect-error TS(2322): Type '{ validator: ((rule: any, value: any, callba... Remove this comment to see the full error message */}
         <CreateExperimentForm validator={this.debouncedExperimentNameValidator} />
